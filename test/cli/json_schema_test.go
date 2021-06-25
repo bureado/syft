d2993d398ba65fd9c1af2dec0e18621ede032bfd
Alex Goodman <alex.goodman@anchore.com>
2021-06-25 16:11:09 -0400

    improve testing around directory resolver
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- test/cli/json_schema_test.go
+++ test/cli/json_schema_test.go
@@ -60,7 +60,7 @@ func TestJSONSchema(t *testing.T) {
 				args = append(args, a)
 			}
 
-			_, stdout, _ := runSyftCommand(t, nil, args...)
+			_, stdout, _ := runSyft(t, nil, args...)
 
 			if len(strings.Trim(stdout, "\n ")) < 100 {
 				t.Fatalf("bad syft output: %q", stdout)
